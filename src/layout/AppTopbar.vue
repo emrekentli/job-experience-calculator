fb3caf152496cb95649e5c727f6172a6adaf8ed2
Emre Kentli <emrekentlii@gmail.com>
2024-05-30 01:57:33 +0300

    init

--- src/layout/AppTopbar.vue
+++ src/layout/AppTopbar.vue
@@ -74,7 +74,7 @@ const isOutsideClicked = (event) => {
 <template>
     <div class="layout-topbar">
         <router-link to="/" class="layout-topbar-logo">
-            <span>Sılapp</span>
+            <span>Bofusu 💕</span>
         </router-link>
 
         <button class="p-link layout-menu-button layout-topbar-button" @click="onMenuToggle()">
